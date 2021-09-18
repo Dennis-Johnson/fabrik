1b1d2d20f2fa93a062c87feae7c0e3f56793d0c7
Dennis Johnson <densarjohn@gmail.com>
2021-09-18 17:02:01 +0530

    Setup

--- fabrik.js
+++ fabrik.js
@@ -1,38 +1,46 @@
-var angle = Math.PI / 6;
-var trees = 7;
-var xoff = 0.02;
-var col = 255;
+let chain;
+
+class Joint{
+  static diameter = 15
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  show() {
+    ellipse(this.x, this.y, Joint.diameter, Joint.diameter);
+  }
+}
+
+class Chain {
+  constructor(numJoints = 2) {
+    this.joints = []
+    
+    this.joints.push(new Joint(100, 200))
+    this.joints.push(new Joint(100, 300))
+    this.joints.push(new Joint(200, 400))
+  }
+
+  show() {
+    for (let i = 0; i < this.joints.length; i++) {
+      this.joints[i].show()
+
+      if(i > 0)
+        line(this.joints[i-1].x, this.joints[i-1].y, this.joints[i].x, this.joints[i].y)
+    }
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // frameRate(1) //framerate adjustment is laggy
-  noLoop()
+
+  background(51);
+  stroke(255);
+  fill(255)
+
+  chain = new Chain(2)
 }
 
 function draw() {
-  background(240);
-  stroke(0);
-  for (var i = 0; i <= trees; i++) {           //shorties
-    push();
-    translate(i * width / trees, height+30);
-    branch(random(60, 110));
-    pop();
-  }
-}
-
-function branch(len) {
-  line(0, 0, 0, -len);
-  translate(0, -len, 0);
-
-  if (len > 1.3) {
-    if (len < 10) stroke(0, 170, 0)
-    push();
-    rotate(map(noise(xoff), 0, 1, Math.PI / 7, angle));
-    branch(len * random(0.60, 0.79));
-    pop();
-    push();
-    rotate(-(map(noise(xoff), 0, 1, Math.PI / 7, angle)));
-    branch(len * random(0.60, 0.79));
-    pop();
-  }
+  chain.show()
 }
